4c5f2e1b970b2b73a48ece7fdb317acee2ed1997
Lev Rosenberg <lev.rosenberg@gmail.com>
2023-10-21 09:42:25 -0500

    insta

--- components/navbar.js
+++ components/navbar.js
@@ -1,5 +1,8 @@
 import styles from '../styles/layout.module.css';
 import Link from 'next/link'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faInstagram } from '@fortawesome/free-brands-svg-icons'
+
 
 export function Navbar() {
 
@@ -21,6 +24,10 @@ export function Navbar() {
         <Link href = "/about">
           <h3>About</h3>
         </Link>
+          <a href = "https://www.instagram.com/adamahceramics/">
+            <FontAwesomeIcon icon={faInstagram} style={{ width: '1rem'}}/>
+          </a>
+        
       </nav>
     </div>
   )
